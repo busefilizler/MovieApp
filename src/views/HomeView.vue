1cae8dcde9c1811d1ea5102611a408eb818440d5
Buse Filizler <b.filizler@digitastic.app>
2023-01-26 15:25:00 +0300

    edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,22 +8,29 @@
         class="w-full absolute object-cover h-full opacity-30 mask"
       />
       <transition name="slide" appear>
-        <div class="DETATILS flex flex-row pl-10 absolute h-full">
+        <div class="flex flex-row pl-10 absolute h-full">
           <div
             class="w-1/2 flex flex-col items-start justify-center ml-24 text-white"
           >
-            <h1 class="text-6xl font-extrabold pb-4">{{ movies[0].title }}</h1>
+            <h1 class="text-6xl font-extrabold pb-4">{{ movies[i].title }}</h1>
             <p class="md:text-lg sm:text-base">
-              {{ movies[0].overview }}
+              {{ movies[i].overview }}
             </p>
             <button
               class="bg-white text-black p-2 px-10 shadow-2xl rounded-full mt-11 text-base font-semibold !cursor-pointer hover:bg-red-600 hover:text-white z-50"
+              @click="watchTrailer"
             >
               Watch Trailer
             </button>
           </div>
-          <div class="w-1/2 hidden lg:flex items-center justify-center mx-24">
-            <img :src="posterPath" class="w-2/4 rounded-xl shadow-2xl" />
+          <div
+            @click="counter"
+            class="w-1/2 hidden lg:flex items-center justify-center mx-24"
+          >
+            <img
+              :src="posterPath"
+              class="w-2/4 rounded-xl shadow-2xl cursor-pointer z-50"
+            />
           </div>
         </div>
       </transition>
@@ -62,13 +69,13 @@ export default {
     return {
       movies: [],
       i: 0,
+      link: "",
     };
   },
   async mounted() {
     const data = await movieService.fetchPopularMovies();
     this.movies = data.results;
-    console.log(this.movies[0]);
-    console.log(this.movies[0]?.backdrop_path);
+    // console.log(this.movies);
   },
   computed: {
     posterPath() {
@@ -76,6 +83,20 @@ export default {
     },
     backdropPath() {
       return IMG_PATH + this.movies[this.i]?.backdrop_path;
+    },
+  },
+  methods: {
+    counter() {
+      this.i = (this.i + 1) % 20;
+    },
+    async watchTrailer() {
+      const { results } = await movieService.fetchMovieTrailerInfo(
+        this.movies[this.i].id
+      );
+      const key = results[0].key;
+      this.link = `https://www.youtube.com/watch?v=${key}`;
+      console.log(this.link);
+      window.open(this.link);
     },
   },
 };
